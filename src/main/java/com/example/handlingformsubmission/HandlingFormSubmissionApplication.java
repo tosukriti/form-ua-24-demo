45ebd2d28ce8ef1ce689647a6d6c89d296f005b4
tosukriti <tosukriti5@gmail.com>
2021-05-19 12:49:07 +0530

    changes

--- src/main/java/com/example/handlingformsubmission/HandlingFormSubmissionApplication.java
+++ src/main/java/com/example/handlingformsubmission/HandlingFormSubmissionApplication.java
@@ -2,10 +2,17 @@ package com.example.handlingformsubmission;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class HandlingFormSubmissionApplication {
+public class HandlingFormSubmissionApplication extends SpringBootServletInitializer{
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(HandlingFormSubmissionApplication.class);
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(HandlingFormSubmissionApplication.class, args);
 	}
